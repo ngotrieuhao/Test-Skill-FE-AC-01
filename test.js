6aea417c970c9a58296725abdc13f9d9d149b5e8
Hao Ngo <hao.ngo@apps-cyclone.com>
2024-01-05 14:10:02 +0700

    update test-skill code

--- test.js
+++ test.js
@@ -27,50 +27,66 @@ teamsData = JSON.parse(teamsJson);
 nationsData = JSON.parse(nationsJson);
 
 function run() {
+  console.log(
+    "Type \x1b[38;2;242;127;36m'quit'\x1b[0m to exit any time. Press \x1b[38;2;0;155;117m'Enter'\x1b[0m to continute"
+  );
+  console.log(" ");
+  runFirstStep();
+}
+
+function runFirstStep() {
   console.log("Select search options:");
   console.log(" - Press 1 to search");
   console.log(" - Press 2 to view a list of searchable fields");
   console.log(
     " - Type \x1b[38;2;242;127;36m'quit'\x1b[0m to \x1b[38;2;0;155;117mexit\x1b[0m"
   );
-  
-
-  rl.question("Choose an option: ", (selectedOption) => {
+  rl.question("", (selectedOption) => {
     // Selected for Search
     if (selectedOption === "1") {
       console.log("Select 1: Players or 2: Teams or 3: Nations");
-      rl.question("Choose an option: ", (secondOption) => {
+      rl.question("", (secondOption) => {
         // Select Players
         if (secondOption === "1") {
           function getNationName(nationId) {
-            const nation = nationsData.filter((n) => n._id === nationId).map((nationName) => nationName.name);
+            const nation = nationsData
+              .filter((n) => n._id === nationId)
+              .map((nationName) => nationName.name);
             return nation;
           }
           function getTeamName(teamId) {
-            const team = teamsData.filter((n) => n._id === teamId).map((teamName) => teamName.name);
+            const team = teamsData
+              .filter((n) => n._id === teamId)
+              .map((teamName) => teamName.name);
             return team;
           }
-
-          rl.question("Enter search term: ", (inputKey) => {
+          console.log("Enter search term");
+          rl.question("", (inputKey) => {
             if (Object.keys(playersData[0]).includes(inputKey)) {
-              rl.question("Enter search value: ", (inputValue) => {
+              console.log("Enter search value");
+              rl.question("", (inputValue) => {
                 let results = [];
                 for (let i = 0; i < playersData.length; i++) {
                   const player = playersData[i];
-                  if (player[inputKey]?.toString().toLowerCase() === inputValue.toLowerCase()) {
+                  if (
+                    player[inputKey]?.toString().toLowerCase() ===
+                    inputValue.toLowerCase()
+                  ) {
                     results.push({
                       ...player,
                       nation_name: getNationName(player.nation_id),
-                      team_name: getTeamName(player.team_id)
+                      team_name: getTeamName(player.team_id),
                     });
                   }
                 }
                 if (results.length > 0) {
-                  if(results.length === 1) {
+                  if (results.length === 1) {
                     const resultObject = results[0];
-                    console.log(resultObject)
+                    console.log("=> Result: ");
+                    console.log(resultObject);
                     rl.close();
                   } else {
+                    console.log("=> Result: ");
                     console.log(results);
                     rl.close();
                   }
@@ -91,31 +107,41 @@ function run() {
             const nation = nationsData.find((n) => n._id === nationId);
             return nation ? nation.name : null;
           }
-        
+
           function getPlayerName(teamId) {
-            const player = playersData.filter((n) => n.team_id === teamId).map((matchingPlayer) => matchingPlayer.name);
+            const player = playersData
+              .filter((n) => n.team_id === teamId)
+              .map((matchingPlayer) => matchingPlayer.name);
             return player;
           }
-          rl.question("Enter search term: ", (inputKey) => {
+
+          console.log("Enter search term");
+          rl.question("", (inputKey) => {
             if (Object.keys(teamsData[0]).includes(inputKey)) {
-              rl.question("Enter search value: ", (inputValue) => {
+              console.log("Enter search value");
+              rl.question("", (inputValue) => {
                 let results = [];
                 for (let i = 0; i < teamsData.length; i++) {
                   const team = teamsData[i];
-                  if (team[inputKey]?.toString().toLowerCase() === inputValue.toLowerCase()) {
+                  if (
+                    team[inputKey]?.toString().toLowerCase() ===
+                    inputValue.toLowerCase()
+                  ) {
                     results.push({
                       ...team,
                       nation_name: getNationName(team.nation_id),
-                      players: getPlayerName(team._id)
+                      players: getPlayerName(team._id),
                     });
                   }
                 }
                 if (results.length > 0) {
-                  if(results.length === 1) {
+                  if (results.length === 1) {
                     const resultObject = results[0];
-                    console.log(resultObject)
+                    console.log("=> Result: ");
+                    console.log(resultObject);
                     rl.close();
                   } else {
+                    console.log("=> Result: ");
                     console.log(results);
                     rl.close();
                   }
@@ -133,33 +159,44 @@ function run() {
           // Select Nations
         } else if (secondOption === "3") {
           function getPlayerName(nationId) {
-            const player = playersData.filter((n) => n.nation_id === nationId).map((matchingPlayer) => matchingPlayer.name);
+            const player = playersData
+              .filter((n) => n.nation_id === nationId)
+              .map((matchingPlayer) => matchingPlayer.name);
             return player;
           }
           function getTeamName(nationId) {
-            const team = teamsData.filter((n) => n.nation_id === nationId).map((teamName) => teamName.name);
+            const team = teamsData
+              .filter((n) => n.nation_id === nationId)
+              .map((teamName) => teamName.name);
             return team;
           }
-          rl.question("Enter search term: ", (inputKey) => {
+          console.log("Enter search term");
+          rl.question("", (inputKey) => {
             if (Object.keys(nationsData[0]).includes(inputKey)) {
-              rl.question("Enter search value: ", (inputValue) => {
+              console.log("Enter search value");
+              rl.question("", (inputValue) => {
                 let results = [];
                 for (let i = 0; i < nationsData.length; i++) {
                   const nation = nationsData[i];
-                  if (nation[inputKey]?.toString().toLowerCase() === inputValue.toLowerCase()) {
+                  if (
+                    nation[inputKey]?.toString().toLowerCase() ===
+                    inputValue.toLowerCase()
+                  ) {
                     results.push({
                       ...nation,
                       team_name: getTeamName(nation._id),
-                      players: getPlayerName(nation._id)
+                      players: getPlayerName(nation._id),
                     });
                   }
                 }
                 if (results.length > 0) {
-                  if(results.length === 1) {
+                  if (results.length === 1) {
                     const resultObject = results[0];
-                    console.log(resultObject)
+                    console.log("=> Result: ");
+                    console.log(resultObject);
                     rl.close();
                   } else {
+                    console.log("=> Result: ");
                     console.log(results);
                     rl.close();
                   }
@@ -182,22 +219,28 @@ function run() {
       // Selected for View
     } else if (selectedOption === "2") {
       console.log("Select 1: Players or 2: Teams or 3: Nations");
-      rl.question("Choose an option: ", (viewOption) => {
+      rl.question("", (viewOption) => {
         // View Players key
         if (viewOption === "1") {
           const keys = Object.keys(playersData[0]);
+          console.log("=> Result: ");
+          console.log("Search Players with");
           console.log(String(keys.join("\n")));
           rl.close();
 
           // View Teams key
         } else if (viewOption === "2") {
           const keys = Object.keys(teamsData[0]);
+          console.log("=> Result: ");
+          console.log("Search Teams with");
           console.log(String(keys.join("\n")));
           rl.close();
 
           // View Nations key
         } else if (viewOption === "3") {
           const keys = Object.keys(nationsData[0]);
+          console.log("=> Result: ");
+          console.log("Search Nations with");
           console.log(String(keys.join("\n")));
           rl.close();
         } else {
@@ -210,8 +253,7 @@ function run() {
     } else if (selectedOption === "quit") {
       rl.close();
     } else {
-      console.log("Invalid option.");
-      rl.close();
+      runFirstStep();
     }
   });
 }
